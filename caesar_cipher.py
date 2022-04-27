b6ca3d330ad468074a21d2cd3b19a7304b5d0feb
Gabriele Abbate <56169712+morebaconstrips@users.noreply.github.com>
2022-04-27 22:22:01 +0200

    add main file

---
+++ caesar_cipher.py
@@ -0,0 +1,38 @@
+import sys
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+def caesar(string, rotations, back):
+	decrypted = ''	
+	for i in range(len(string)):
+		if string[i].lower() not in alphabet:
+			decrypted += string[i]
+			continue
+		if back:
+			decrypted += alphabet[(alphabet.index(string[i].lower()) - rotations) % 26]
+		else:
+			decrypted += alphabet[(alphabet.index(string[i].lower()) + rotations) % 26]
+	print(f'Result: {decrypted}')
+
+def print_usage():
+    print('Usage: python3 caesar_cipher.py [message] [rotations] [back=False]')
+		
+
+if __name__ == "__main__":
+	try:
+		encrypter_message = sys.argv[1]
+		rotations = int(sys.argv[2])
+	except IndexError:
+		print_usage()
+		exit(0)
+	try:
+		back = eval(sys.argv[3])
+		caesar(encrypter_message, rotations, back)
+	except NameError:
+		print('3rd parameter must be [True/False]')
+		print_usage()
+		exit(0)
+	except IndexError:
+		print('"back" parameter set to [False] by default')
+		caesar(encrypter_message, rotations, False)
